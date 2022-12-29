74d187f124b1c8cba1e9328652582202465994cb
MohamedSalah15 <121621712+MohamedSalah15@users.noreply.github.com>
2022-12-29 16:20:34 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FST Num");
+            int xo = int.Parse(Console.ReadLine());   
+            Console.WriteLine("SCD Num");
+            int ox = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for(int x=xo;x<=ox;x++)
+            {
+                sum = 0;
+                for (int l = 1; l <= x / 2; l++)
+                {
+                    if (x % l == 0)
+                        sum += l;              
+                }
+                if (sum == x)
+                    Console.WriteLine( x + " Perfect Number");
+            }
+
+
+
+
+
+        }
+    }
+}
